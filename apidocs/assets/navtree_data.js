e176ad673fb3342bae9690177771a87cb653729e
Simo Tripodi <stripodi@adobe.com>
2013-02-15 17:43:47 +0100

    Wagon: Deploying site to repository

---
+++ apidocs/assets/navtree_data.js
@@ -0,0 +1,7 @@
+var NAVTREE_DATA =
+[ [ "org.mybatis.caches.memcached", "org/mybatis/caches/memcached/package-summary.html", [ [ "Description", "org/mybatis/caches/memcached/package-descr.html", null, "" ], [ "Classes", null, [ [ "LoggingMemcachedCache", "org/mybatis/caches/memcached/LoggingMemcachedCache.html", null, "" ], [ "MemcachedCache", "org/mybatis/caches/memcached/MemcachedCache.html", null, "" ] ]
+, "" ] ]
+, "" ] ]
+
+;
+
---
+++ apidocs/reference/lists.js
@@ -0,0 +1,6 @@
+var DATA = [
+      { id:0, label:"org.mybatis.caches.memcached", link:"org/mybatis/caches/memcached/package-summary.html", type:"package" },
+      { id:1, label:"org.mybatis.caches.memcached.LoggingMemcachedCache", link:"org/mybatis/caches/memcached/LoggingMemcachedCache.html", type:"class" },
+      { id:2, label:"org.mybatis.caches.memcached.MemcachedCache", link:"org/mybatis/caches/memcached/MemcachedCache.html", type:"class" }
+
+    ];
