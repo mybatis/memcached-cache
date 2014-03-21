852ad0f8642975948098b5359761b18d2ba4e1be
Eduardo Macarron <eduardo.macarron@gmail.com>
2014-03-21 17:53:09 +0100

    Wagon: Deploying site to repository

--- apidocs/assets/navtree_data.js
+++ apidocs/assets/navtree_data.js
@@ -1,5 +1,5 @@
 var NAVTREE_DATA =
-[ [ "org.mybatis.caches.memcached", "org/mybatis/caches/memcached/package-summary.html", [ [ "Description", "org/mybatis/caches/memcached/package-descr.html", null, "" ], [ "Classes", null, [ [ "LoggingMemcachedCache", "org/mybatis/caches/memcached/LoggingMemcachedCache.html", null, "" ], [ "MemcachedCache", "org/mybatis/caches/memcached/MemcachedCache.html", null, "" ] ]
+[ [ "org.mybatis.caches.memcached", "org/mybatis/caches/memcached/package-summary.html", [ [ "Description", "org/mybatis/caches/memcached/package-descr.html", null, "" ], [ "Classes", null, [ [ "LoggingMemcachedCache", "org/mybatis/caches/memcached/LoggingMemcachedCache.html", null, "" ], [ "MemcachedCache", "org/mybatis/caches/memcached/MemcachedCache.html", null, "" ], [ "StringUtils", "org/mybatis/caches/memcached/StringUtils.html", null, "" ] ]
 , "" ] ]
 , "" ] ]
 
--- apidocs/reference/lists.js
+++ apidocs/reference/lists.js
@@ -1,6 +1,7 @@
 var DATA = [
       { id:0, label:"org.mybatis.caches.memcached", link:"org/mybatis/caches/memcached/package-summary.html", type:"package" },
       { id:1, label:"org.mybatis.caches.memcached.LoggingMemcachedCache", link:"org/mybatis/caches/memcached/LoggingMemcachedCache.html", type:"class" },
-      { id:2, label:"org.mybatis.caches.memcached.MemcachedCache", link:"org/mybatis/caches/memcached/MemcachedCache.html", type:"class" }
+      { id:2, label:"org.mybatis.caches.memcached.MemcachedCache", link:"org/mybatis/caches/memcached/MemcachedCache.html", type:"class" },
+      { id:3, label:"org.mybatis.caches.memcached.StringUtils", link:"org/mybatis/caches/memcached/StringUtils.html", type:"class" }
 
     ];
