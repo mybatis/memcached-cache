c64a50e391264943c5738b18daa478fc1422d828
hazendaz <jeremylandis@hotmail.com>
2026-01-01 20:35:06 +0000

    Deploying to gh-pages from @ mybatis/memcached-cache@06118e23e98640365f7f06c08fe4e80f86410aed 🚀

--- apidocs/type-search-index.js
+++ apidocs/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"org.mybatis.caches.memcached","l":"LoggingMemcachedCache"},{"p":"org.mybatis.caches.memcached","l":"MemcachedCache"},{"p":"org.mybatis.caches.memcached","l":"StringUtils"}];updateSearchResults();
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html","k":"18"},{"p":"org.mybatis.caches.memcached","l":"LoggingMemcachedCache"},{"p":"org.mybatis.caches.memcached","l":"MemcachedCache"},{"p":"org.mybatis.caches.memcached","l":"StringUtils"}];updateSearchResults();
